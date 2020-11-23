b0ae750fec28f78e484c18317c813df642cd8900
jody29 <66092262+jody29@users.noreply.github.com>
2020-11-23 23:47:16 +0100

    Commit1 - Jody Lorist - Log In scherm

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>phpgram</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/style.css">
+</head>
+<body id="loginPage">
+    
+    <header>
+        
+    </header>
+    
+    <main>
+      
+      <h1>phpgram</h1>
+       
+        <form action="" method="post">
+            
+            <input type="text" placeholder="username">
+            
+            <input type="password" placeholder="password">
+            
+            <input type="submit" value="Log In">
+            
+        </form>
+        
+        <a href="">Don't have an account? <b>Sign up.</b></a>
+        
+    </main>
+    
+    <footer>
+        
+        
+    </footer>
+    
+    
+</body>
+</html>
