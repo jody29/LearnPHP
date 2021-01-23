d5e9392a1f7356df71d7bf8f5622cfb7b7afd88b
jody29 <66092262+jody29@users.noreply.github.com>
2021-01-23 17:25:12 +0100

    grid added + empty state

--- accountPage.php
+++ accountPage.php
@@ -11,7 +11,7 @@ $username = $_SESSION["username"];
     <meta charset="UTF-8">
     <title><?= $_SESSION["username"]; ?> Page</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../CodeGram-main/styles/style.css">
+    <link rel="stylesheet" href="styles/style.css">
     
 </head>
 <body id="AccountPage">
